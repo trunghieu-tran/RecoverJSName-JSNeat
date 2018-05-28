6a9b23d0a9ab991c8b020cc7f0e23d30e8b12241
miketran238 <ngocmike238@gmail.com>
2018-05-28 18:34:26 -0500

    Minor fix

---
+++ resources/_test/test.js
@@ -0,0 +1,14 @@
+var person = {
+	firstName: "John",
+	lastName : "Doe",
+	id       : 5566,
+	wholeName : function(fn, ln) {
+		return fn + " " + ln;
+	}
+};
+var x;
+x = person.firstName;
+var y = person.lastName;
+y = x;
+var z = person.wholeName(x, y);
+z = x + y;
--- src/main/java/parser/MainParser.java
+++ src/main/java/parser/MainParser.java
@@ -13,7 +13,9 @@ public class MainParser {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		String filePath = ".\\resources\\test.js";
+		String filePath = "../Data/";
+//		String filePath = "F:\\Study\\Research\\RecoverJsName\\Data\\0xsky\\xblog\\xblogroot\\admin\\js\\admin.js";
+//		String filePath = ".\\resources\\_test";
 		MainParser demo = new MainParser();
 		demo.parseJS(filePath);
 	}
@@ -23,23 +25,36 @@ public class MainParser {
 		CompilerEnvirons env = new CompilerEnvirons();
 		env.setRecoverFromErrors(true);
 		MyVisitor myVisitor = new MyVisitor();
-		File dir = new File("../Data");
+		File dir = new File(filePath);
 		ArrayList<File> files = new ArrayList<>();
 		searchDir(dir, files);
 		//File[] files = dir.listFiles();
 		for ( File file : files )
 		{
 			System.out.println(file.getAbsolutePath());
-			FileReader strReader = new FileReader(filePath);
+			FileReader strReader = new FileReader(file.getAbsolutePath());
 			IRFactory factory = new IRFactory(env, new JSErrorReporter());
+			try
+			{
 			AstRoot rootNode = factory.parse(strReader, null, 0);
 			rootNode.visit(myVisitor);
+			}
+			catch (Exception e)
+			{
+				System.out.println("Exception at " + e);
+				continue;
+			}
 		}
 		myVisitor.print();
 		myVisitor.printToFile();
 	}
 
 	private void searchDir(File dir, ArrayList<File> files) {
+		if ( dir.isFile() )
+		{
+			files.add(dir);
+			return;
+		}
 		for ( File file : dir.listFiles() )
 		{
 			if ( file.isDirectory() )
@@ -48,7 +63,10 @@ public class MainParser {
 			}
 			else if ( file.getName().contains(".js") )
 			{
-				files.add(file);
+				if ( !file.getName().startsWith("._"))
+				{
+					files.add(file);
+				}
 			}
 		}
 	}	
