6bfc9436cf03651e8db02c188b990c86b363fcf6
hieutran <trunghieu.tran@utdallas.edu>
2018-08-03 23:19:05 -0500

    Re-implement association var-var

---
+++ src/main/java/association/AssociationVarVar.java
@@ -0,0 +1,10 @@
+package association;
+
+/**
+ * @author Harry Tran on 8/3/18.
+ * @project RecoverJSName
+ * @email trunghieu.tran@utdallas.edu
+ * @organization UTDallas
+ */
+public class AssociationVarVar {
+}
